b26f0aa907c59b7b33c0c6be878f06629394bedf
Abhinash Mishra <abhimishrabhi2580@gmail.com>
2025-05-11 00:28:41 +0530

    daily leetcode

---
+++ leetcode110.java
@@ -0,0 +1,28 @@
+public class leetcode110 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }public boolean isBalanced(TreeNode root) {
+        return helper(root)!=-1;
+    }public int helper(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+        int left=helper(root.left);
+        if (left == -1) return -1;
+        int right=helper(root.right);
+        if (right == -1) return -1;
+        if(Math.abs(left-right)>1){
+            return -1;
+        }
+        return 1+Math.max(left,right);
+    }
+}
