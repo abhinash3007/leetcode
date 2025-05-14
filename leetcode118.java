43e1fc315c7991eae88f4a6c0aeb28f776a0f783
Abhinash Mishra <abhimishrabhi2580@gmail.com>
2025-05-15 00:18:53 +0530

    daily leetcode

---
+++ leetcode118.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import org.w3c.dom.Node;
+
+public class leetcode118 {
+    public List<List<Integer>> getRow(int rowIndex) {
+        List<List<Integer>> list = new ArrayList<>();
+        for (int i = 0; i <=rowIndex; i++) {
+            List<Integer> ans = new ArrayList<>();
+            for (int j = 0; j <= i; j++) {
+                if (j == 0) {
+                    ans.add(1);
+                } else if (j == i) {
+                    ans.add(1);
+                } else {
+                    int a = list.get(i - 1).get(j - 1);
+                    int b = list.get(i - 1).get(j);
+                    int c = a + b;
+                    ans.add(c);
+                }
+            }
+            list.add(ans);
+        }
+        return list;
+    }
+}
